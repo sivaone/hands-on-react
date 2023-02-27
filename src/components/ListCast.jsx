62899726054634a21f464cc08078b96b28439abf
Siva Nagi Reddy Alla <sivaone@users.noreply.github.com>
2023-02-27 16:50:58 +0000

    Task completed

---
+++ src/components/ListCast.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+export default () => {
+  const [cast, setCast] = useState([]);
+
+  async function fetchCast() {
+    const response = await fetch('cast.json');
+    setCast(await response.json());
+  }
+
+  useEffect(() => {
+    fetchCast();
+  });
+
+  return (
+    <div style={{
+      display: "grid",
+      gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
+      gap: `1rem`,
+      marginBottom: `1rep`
+    }}>
+      {
+        cast.map(member => (
+          <a key={member.id} data-tooltip={member.name}>
+            <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
+          </a>
+        ))
+      }
+    </div>
+  );
+}
