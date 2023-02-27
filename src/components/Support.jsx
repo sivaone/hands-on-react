914d9b82de4842d269893c2786295eb0cb6eb52b
Siva Nagi Reddy Alla <sivaone@users.noreply.github.com>
2023-02-27 16:37:16 +0000

    task complete

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const pageTitle = document.title;
+
+useEffect(() => {
+  if (count > 0) {
+    document.title = `${pageTitle}--${count}`;
+  }
+  // consise
+  // count && (document.title = `${pageTitle}--${count}`);
+});
 
 export default () => {
   const [count, setCount] = useState(0)
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
-      {count === 0 ? "Click to support": `Supported ${count} times`}
+      {count === 0 ? "Click to support" : `Supported ${count} times`}
     </button>
   )
 }
