5e055a87017fcc9c1d34eb0fcf242423b2ddb908
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2023-02-27 16:00:16 -0600

    task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Welcome from './components/Welcome';
-import Support from './components/Support';
 import ListCast from './components/ListCast';
 
 function App() {
-  const name = 'StarGazers'
+  const name = 'StarGazers';
+  let [memberInfo, setMemberInfo] = useState(null);
+
   return (
     <div className="container">
-      <article>
-        <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          {/* <ListCast /> */}
-          <Support />
-        </hgroup>
-      </article>
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+        <Welcome name="StarGazers" />
+        <p>Members of an <b>intergalactic alliance</b><br />
+          paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        <ListCast onChoice={(info) => { setMemberInfo(info) }} />
+        {
+          memberInfo &&
+          <article><hgroup>
+            <div>
+              <img src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
+              <h2>{memberInfo.name}</h2>
+              <h2>{memberInfo.bio}</h2>
+            </div>
+          </hgroup></article>
+        }
+      </hgroup>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 3000
   },
   plugins: [react()]
