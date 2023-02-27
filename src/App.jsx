92ebff44466aa92ba0aa506bb0655a7ef50fdd4a
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2023-02-27 16:39:48 -0600

    task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react';
 import ListCast from './components/ListCast';
+import Modals from './components/Modals';
 
 function App() {
   const name = 'StarGazers'
@@ -12,20 +13,7 @@ function App() {
         <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
         <ListCast onChoice={(info) => { setMemberInfo(info) }} />
         {memberInfo &&
-          <article>
-            <hgroup>
-              <div style={{
-                display: "flex",
-                gap: '1rem'
-              }}>
-                <img style={{ width: '200px' }} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
-                <hgroup>
-                  <h1>{memberInfo.name}</h1>
-                  <p>{memberInfo.bio}</p>
-                </hgroup>
-              </div>
-            </hgroup>
-          </article>
+          <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />
         }
       </hgroup>
     </div>
